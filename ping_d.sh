714b3c4c55f090107da4e0bed9a3480af80fd86f
sam <bisou@sams-MacBook-Pro.local>
2021-01-05 10:14:50 +0100

    updates to show how long connection was off

--- ping_d.sh
+++ ping_d.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-trap '{ echo "Ping deamon stopped." ; exit 1; }' INT
+trap '{ echo "Ping deamon stopped." ; echo "SCRIPT ENDING (properly)" >> $file_name; exit 1;}' INT
 echo "Monitoring internet connection, press Ctrl-C to interrupt."
 
 PING_STATUS="CONNECTED"
+
+DISCONNECTION_TIME=0
 
 file_name=$(date '+%d_%m_%Y-%H_%M_%S');
 file_name="pinglogs_$file_name.txt";
@@ -14,8 +16,14 @@ log_connection_status()
    NEW_STATUS=$1
    if [[ "$NEW_STATUS" != "$PING_STATUS" ]]
    then
+      
+	if [[ "$NEW_STATUS" == "CONNECTED" ]]
+	then
+	    
+	    DURATION_MESSAGE="(was offline during $DISCONNECTION_TIME seconds)"
+	fi
       dt=$(date '+%d/%m/%Y %H:%M:%S');
-      echo "$dt : $NEW_STATUS" >> $file_name
+      echo "$dt : $NEW_STATUS $DURATION_MESSAGE" >> $file_name
       PING_STATUS=$NEW_STATUS
    fi
 }
@@ -27,11 +35,13 @@ do
    dt=$(date '+%d/%m/%Y %H:%M:%S');
    if ping -q -c 1 siim.ml &>/dev/null
    then
-      echo -ne "\r$dt status: CONNECTED"
+      echo -ne "\r$dt status: CONNECTED   "
       log_connection_status "CONNECTED"
+      DISCONNECTION_TIME=0
    else
       echo -ne "\r$dt status: DISCONNECTED"
       log_connection_status "DISCONNECTED"
+      ((DISCONNECTION_TIME+=1))
    fi
 done
 exit 0
